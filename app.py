10703614add0dca8304e634d98aa35bb9bc2cd10
wsyai98 <wsyai9876@gmail.com>
2026-01-30 06:57:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -256,7 +256,7 @@ def compute_table(inputs: dict):
 # ======================== TOP BAR ========================
 logo_paths = [
     APP_DIR / "logo_kpkt.png",
-    APP_DIR / "logo_kementerian_ekonomi.png",
+    APP_DIR / "logo_kementerian_ekonomi.jpg",
     APP_DIR / "logo_uitm.png",
     APP_DIR / "logo_ukm.png",
 ]
